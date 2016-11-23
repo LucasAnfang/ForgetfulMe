a5a4dbeb6c344e9de2e22ff93c38374582d5f380
Richard Anfang <ranfang@optonline.net>
2016-11-22 21:29:29 -0500

    Initial Commit

---
+++ Grip/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Grip
+//
+//  Created by luke anfang on 11/22/16.
+//  Copyright © 2016 luke anfang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Grip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Grip
+//
+//  Created by luke anfang on 11/22/16.
+//  Copyright © 2016 luke anfang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
